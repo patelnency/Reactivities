565736fba486f77216dfb25c8f1ab6e1e8db3e10
Nency Patel <39882426+patelnency@users.noreply.github.com>
2021-05-03 14:36:40 -0400

    End of section 2

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] // api/activivities 
+    public class BaseApiController : ControllerBase
+    {
+        
+    }
+}
